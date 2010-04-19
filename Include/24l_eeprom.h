1ddbad334fd248ec90c40052e4521e0d689f482d
David Cannings <david@edeca.net>
2010-04-19 22:09:52 +0100

    Update comments in 24LC EEPROM driver

--- Include/24l_eeprom.h
+++ Include/24l_eeprom.h
@@ -12,6 +12,12 @@
 #define _24L_EEPROM_H
 
 // Check if the EEPROM is busy (still writing)
+//
+// Arguments:
+//   control_byte - the I2C write message with the correct address
+//
+// Returns:
+//   TRUE if the EEPROM is still writing, FALSE otherwise
 char ee24_is_busy(char control_byte) 
 {
 	i2c_start();
@@ -22,7 +28,18 @@ char ee24_is_busy(char control_byte)
 
 // Sequential write to a specific address
 // Uses the page write mechanism to write up to 64 bytes
-// Blocks if the EEPROM is already writing from a previous operation
+//
+// Arguments:
+//   device_address - 3 byte address of the EEPROM (a2/a1/a0)
+//     data_address - where in the EEPROM to write data
+//             data - pointer to a char buffer
+//            count - how many bytes to write (0<count<=64)
+//
+// Returns: 
+//   Nothing
+//
+// Note: Blocks if the EEPROM is busy completing a previous operation
+//       until the write has finished
 void ee24_write_sequential(char device_address, short data_address, char *data, unsigned char count)
 {
 	// TODO: ACK is returned by i2c_write, we should probably block until it has finished
@@ -53,7 +70,7 @@ void ee24_write_sequential(char device_address, short data_address, char *data, 
 	i2c_stop();
 }
 
-// Byte write to a specific address
+// Write a single byte to a specific address
 void ee24_write_byte(char device_address, short data_address, char byte)
 {
 	ee24_write_sequential(device_address, data_address, &byte, 1);
@@ -62,6 +79,12 @@ void ee24_write_byte(char device_address, short data_address, char byte)
 // Sequential read from a specific address
 // Can read the whole device in this mode, but reads past the highest address will roll over
 //
+// Arguments:
+//   see ee24_write_sequential
+//
+// Returns:
+//   Nothing, data is written directly to data buffer
+// 
 // Note: Does not append a trailing NULL byte, so remember this if reading strings
 void ee24_read_sequential(char device_address, short data_address, char *data, unsigned short count)
 {
@@ -87,7 +110,7 @@ void ee24_read_sequential(char device_address, short data_address, char *data, u
 	i2c_stop();
 }
 
-// Byte write to a specific address
+// Write a single byte from a specific address
 void ee24_read_byte(char device_address, short data_address, char byte)
 {
 	ee24_read_sequential(device_address, data_address, &byte, 1);
