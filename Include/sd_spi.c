553d88d1f1c06a9f49abc318f68938490b3d5f10
David Cannings <david@edeca.net>
2013-12-07 20:52:19 +0000

    Add development code for SD card library using the SPI protocol.
    Features working so far include: card initialisation, commands, block reads, register reads (CID and CSD) and CRC7.
    
    Requires an external SPI implementation with a number of basic functions, see extern definitions in sd_spi.h.

---
+++ Include/sd_spi.c
@@ -0,0 +1,273 @@
+// Replace with whatever header provides your pin definitions
+#include <xc.h>
+#include <string.h>
+#include <stdint.h>
+#include "sd_spi.h"
+// TODO: Consider how to do delays nicely in this library
+#include "delay.h"
+
+// DEBUGGING ONLY - printf in here
+#include <stdio.h>
+
+// TODO: Return a good/bad value
+//
+// Setup the block read with the appropriate command, then read the 16 byte block
+uint16_t sd_read_register(uint8_t cmd, uint8_t *buffer) {
+    memset(buffer, 0, 16);
+    // We can reuse the output buffer here, both CMD9 and CMD10 take stuff bits
+    // that can be any value.
+    cmd = cmd | SD_KEEP_CARD_SELECTED;
+    sd_command(cmd, buffer, R1);
+    
+    // TODO: Check we get the right amount of bytes back, or error
+    uint16_t bytes_read = sd_block_read(buffer, 16);
+    // TODO: Check CRC instead of fake 2 byte read
+    // TODO: Deselect card before 2 byte extra?
+    spi_idle(4);
+    sd_stop();
+    
+    // TODO - We could just return 1/0 here by checking bytes_read == 16, so
+    // long as this function is never used for lengths <> 16
+    return bytes_read;
+}
+
+#ifdef SD_CRC_ENABLED
+uint8_t sd_crc7(uint8_t crc, uint8_t data) {
+    uint8_t len = 8;
+    uint8_t tmp;
+
+    while (len) {
+        tmp = crc & 0x40;
+        if (data & 0x80)
+            tmp ^= 0x40;
+
+        data <<= 1;
+        crc <<= 1;
+
+        if (tmp)
+            crc ^= 0x09;
+
+        len--;
+    }
+
+    return crc;
+}
+#endif
+
+// This can be used for all commands as all response
+// types start with the same status byte
+//
+// If MSB of cmd is set then it will not deassert the card.  This is good
+// for setting up block reads - but the caller is responsible for deselecting
+// the card.
+uint8_t sd_command(unsigned char cmd, unsigned char *argument, uint8_t response_len) {
+    uint8_t status = 0;
+    uint8_t crc = 0;
+    uint8_t timeout = SD_CMD_TIMEOUT;
+    uint8_t cmd_byte = (cmd & 0x7F) | 0x40;
+
+    //printf("Sending command CMD%02u\r\n", cmd);
+    sd_start();
+
+#ifdef SD_CRC_ENABLED
+    crc = sd_crc7(crc, cmd_byte);
+    crc = sd_crc7(crc, argument[3]);
+    crc = sd_crc7(crc, argument[2]);
+    crc = sd_crc7(crc, argument[1]);
+    crc = sd_crc7(crc, argument[0]);
+    crc <<= 1;
+    crc |= 1;
+#else
+    // If CRC isn't enabled then we need to manually calculate the correct
+    // values for CMD0 and CMD8, which always require a valid CRC.
+    if (cmd_byte == 0x40)
+        crc = 0x95;
+    else if (cmd_byte == 0x48)
+        // This CRC value for CMD8 is only valid with the default arguments
+        // sent by sd_initialise().
+        crc = 0x69;
+#endif
+
+    // Send the command
+    spi_byte(cmd_byte);
+
+    // TODO - see if passing a null pointer for "no argument" and branching
+    // on it results in more efficient code.
+    // Send the argument
+    spi_byte(argument[3]);
+    spi_byte(argument[2]);
+    spi_byte(argument[1]);
+    spi_byte(argument[0]);
+
+    // Send CRC
+    spi_byte(crc);
+
+    // Wait until the SD card is ready, by checking the MSB of the byte
+    // received.  This  will always be 0 in a valid R1 response.
+    do {
+        status = spi_byte(0xFF);
+        timeout--;
+    } while ((status & 0x80) && timeout);
+
+    // TODO: Set last error here in a global sd status struct
+    if (timeout == 0) return SD_ERROR_TIMEOUT;
+
+    // DEBUG
+    //if (status & MSK_CRC_ERR) printf(" - CRC error!\r\n");
+
+    while (--response_len) {
+        *argument++ = spi_byte(0xFF);
+    }
+
+    // Edit: Check the todo still but leaving this in here messes up a block
+    // read for CID to a 4GB kingston card.
+    // If this is required we will need to invent a "start transaction" and
+    // "stop transaction" macro that idle the bus for 2 cycles before/after
+    // command sequences.
+    // 4GB SanDisk card absolutely needs this in order to operate.  Odd, or the
+    // XC8 compiler is doing unusual things.
+    if ((cmd & SD_KEEP_CARD_SELECTED) == 0) {
+        sd_stop();
+    }
+
+    // We always return the status byte (equivalent to R1)
+    return status;
+}
+
+void inline sd_stop() {
+    // @todo I definitely read that you should give SD cards more cycles
+    //       but where?  And should CS be deasserted?
+    // Good scope diagram here: http://elm-chan.org/docs/mmc/mmc_e.html which
+    // shows why an extra byte should be given after each command, in order to
+    // release the SPI bus.
+    //
+    SD_CS = 1;
+    spi_idle(2);
+}
+
+void inline sd_start() {
+    SD_CS = 0;
+}
+
+// Returns the number of bytes read, or 0 on error.
+uint16_t sd_block_read(uint8_t *dest, uint16_t count) {
+    uint16_t bytes_read = 0;
+    uint8_t timeout = SD_CMD_TIMEOUT;
+
+    // Wait for the start of block token with timeout.
+    // When tested on a 4GB Sandisk card there was an extra FF byte between R1
+    // and 0xFE token.
+    do {
+        timeout--;
+    } while (spi_byte(0xFF) != SD_TOKEN_START_BLOCK && timeout);
+
+    if (timeout == 0) return 0;
+
+    while (count--) {
+        *dest++ = spi_byte(0xFF);
+        bytes_read++;
+    }
+
+    // TODO: Read two more bytes (CRC16) and check them
+
+    return bytes_read;
+}
+
+// Returns: 0 for failure (see the last error field), 1 for success
+// If this returns 1 then you can switch SPI implementation to high speed mode
+
+uint8_t sd_initialize() {
+    unsigned char status;
+    uint8_t timeout;
+    // Used for request arguments and response data
+    unsigned char data[5] = {0, 0, 0, 0, 0};
+
+    // Send a minimum of 74 clock cycles to allow the SD card to
+    // initialise itself.
+    sd_start();
+    spi_idle(100);
+    sd_stop();
+
+    // CMD0
+    // @todo Bail on timeouts, they aren't good for us
+    status = sd_command(CMD0, data, CMD0_R);
+    if (status == SD_ERROR_TIMEOUT) {
+        // TODO: Set last error here
+        return 0;
+    }
+
+    // Send CMD8, if it's illegal then v1 card, if not v2.  Mandatory as of
+    // SPI specification v2.  We send a VHS of 1 (2.7-3.6v) and check pattern
+    // of 0xA5 (this can be any value).
+    // CMD8 *always needs correct CRC*
+    sd_pack_argument(data, 0x1A5);
+    status = sd_command(CMD8, data, CMD8_R);
+
+    if (status & MSK_ILL_CMD) {
+        // Version 1 initialisation
+        // TODO: Set v1 card in struct
+        //printf("CMD8 illegal, v1 card\r\n");
+    } else {
+        // Version 2 initialisation
+        // TODO: Set v2 card in struct
+        // TODO: Is there anything else in CMD8 response that we need to check?
+
+        if (data[2] != 0x01) {
+            // TODO: Set error and return
+            //printf("voltage range unsupported\r\n");
+            return 0;
+        }
+
+        // @todo Set error and return
+        if (data[3] != 0xA5) {
+            // TODO: Set error and return
+            //printf("check pattern does not match\r\n");
+            return 0;
+        }
+    }
+
+    timeout = 255;
+
+    // Check that the operating voltage is supported by the card by reading
+    // the OCR.
+    // @todo Check voltage range is legal for the system Vdd
+    // @todo Check for illegal command, which indicates "not an SD card"
+    status = sd_command(CMD58, data, CMD58_R);
+    //printf("Status from CMD58: %02x\r\n", status);
+
+    // ACMD41 to start initialisation, wait for it to finish or timeout.
+    // Set bit 30 (HCS) to indicate we support SDHC/SDXC.
+    sd_pack_argument(data, 0x40000000);
+    do {
+        // Arguments to CMD55 can be "stuff bits" rather than reserved bits, so
+        // use same data as ACMD41.
+        sd_command(CMD55, data, CMD55_R);
+        status = sd_command(ACMD41, data, ACMD41_R) & MSK_IDLE;
+        DelayMs(1);
+        timeout--;
+    } while (status && timeout);
+
+    // Timeout whilst waiting for the initialisation process to complete
+    if (timeout == 0) {
+        // Set last error to SD_ERROR_TIMEOUT
+        return 0;
+    }
+
+    return 1;
+}
+
+void sd_pack_argument(uint8_t *argument, uint32_t value) {
+    argument[0] = (uint8_t) value;
+    value >>= 8;
+    argument[1] = (uint8_t) value;
+    value >>= 8;
+    argument[2] = (uint8_t) value;
+    value >>= 8;
+    argument[3] = (uint8_t) value;
+}
+
+unsigned char sd_set_blocklen(unsigned long length, unsigned char *response) {
+    unsigned char argument[4];
+    sd_pack_argument(argument, length);
+    return (sd_command(CMD16, argument, CMD16_R));
+}
---
+++ Include/sd_spi.h
@@ -0,0 +1,210 @@
+#define R1 1
+#define R1B 1
+#define R2 2
+#define R3 5
+
+// R1 + 4 bytes
+#define R7 5
+
+// There are no commands > 127, so we use MSB to indicate
+// that the chip should not be deselected by spi_command().
+// This is used to setup block reads without toggling the
+// chip select in the middle, which is not valid and is rejected
+// by some cards.
+#define SD_KEEP_CARD_SELECTED 0x80
+
+// MSB is not used in the status register, so we use it to
+// indicate a hardware error
+#define SD_ERROR_TIMEOUT 0x80
+
+#define SD_BLOCKSIZE 512
+
+#define MSK_IDLE 0x01
+#define MSK_ERASE_RST 0x02
+#define MSK_ILL_CMD 0x04
+#define MSK_CRC_ERR 0x08
+#define MSK_ERASE_SEQ_ERR 0x10
+#define MSK_ADDR_ERR 0x20
+#define MSK_PARAM_ERR 0x40
+
+// This token precedes any block reads and writes
+// See section 7.3.3.2
+#define SD_TOKEN_START_BLOCK 0xFE
+
+/*#define SD_TOK_WRITE_STARTBLOCK 0xFE
+#define SD_TOK_READ_STARTBLOCK_M 0xFE
+#define SD_TOK_WRITE_STARTBLOCK_M 0xFC
+#define SD_TOK_STOP_MULTI 0xFD*/
+
+/* Error token is 111XXXXX */
+#define MSK_TOK_DATAERROR 0xE0
+/* Bit fields */
+#define MSK_TOK_ERROR 0x01
+#define MSK_TOK_CC_ERROR 0x02
+#define MSK_TOK_ECC_FAILED 0x04
+#define MSK_TOK_CC_OUTOFRANGE 0x08
+#define MSK_TOK_CC_LOCKED 0x10
+/* Mask off the bits in the OCR corresponding to voltage range 3.2V to
+* 3.4V, OCR bits 20 and 21 */
+#define MSK_OCR_33 0xC0
+/* Number of times to retry the probe cycle during initialization */
+#define SD_INIT_TRY 50
+/* Number of tries to wait for the card to go idle during initialization */
+#define SD_IDLE_WAIT 100
+
+#define SD_CMD_TIMEOUT 32
+/******************************** Basic command set **************************/
+/* Reset cards to idle state */
+#define CMD0 0
+#define CMD0_R R1
+/* Read the OCR (MMC mode, do not use for SD cards) */
+#define CMD1 1
+#define CMD1_R R1
+
+/* Ask card if it can operate in supplied voltage range */
+/* Arguments: host voltage, only one bit may be set at a time */
+/*            check pattern can be any 8 bit pattern */
+#define CMD8 8
+#define CMD8_R R7
+
+/* Card sends the CSD */
+#define CMD9 9
+#define CMD9_R R1
+/* Card sends CID */
+#define CMD10 10
+#define CMD10_R R1
+/* Stop a multiple block (stream) read/write operation */
+#define CMD12 12
+#define CMD12_R R1B
+/* Get the addressed card's status register */
+#define CMD13 13
+#define CMD13_R R2
+
+/* Block read commands */
+
+#define CMD16 16
+#define CMD16_R R1
+/* Read a single block */
+#define CMD17 17
+#define CMD17_R R1
+
+/* Read multiple blocks until a CMD12 */
+#define CMD18 18
+#define CMD18_R R1
+
+
+/***************************** Block write commands *************************/
+/* Write a block of the size selected with CMD16 */
+#define CMD24 24
+#define CMD24_R R1
+/* Multiple block write until a CMD12 */
+#define CMD25 25
+#define CMD25_R R1
+/* Program the programmable bits of the CSD */
+#define CMD27 27
+#define CMD27_R R1
+/***************************** Write protection *****************************/
+/* Set the write protection bit of the addressed group */
+#define CMD28 28
+#define CMD28_R R1B
+/* Clear the write protection bit of the addressed group */
+#define CMD29 29
+#define CMD29_R R1B
+/* Ask the card for the status of the write protection bits */
+#define CMD30 30
+#define CMD30_R R1
+/***************************** Erase commands *******************************/
+/* Set the address of the first write block to be erased */
+#define CMD32 32
+#define CMD32_R R1
+/* Set the address of the last write block to be erased */
+#define CMD33 33
+#define CMD33_R R1
+/* Erase the selected write blocks */
+#define CMD38 38
+#define CMD38_R R1B
+/***************************** Lock Card commands ***************************/
+/* Commands from 42 to 54, not defined here */
+/***************************** Application-specific commands ****************/
+/* Flag that the next command is application-specific */
+#define CMD55 55
+#define CMD55_R R1
+/* General purpose I/O for application-specific commands */
+#define CMD56 56
+#define CMD56_R R1
+/* Read the OCR (SPI mode only) */
+#define CMD58 58
+#define CMD58_R R3
+/* Turn CRC on or off */
+#define CMD59 59
+#define CMD59_R R1
+/***************************** Application-specific commands ***************/
+/* Get the SD card's status */
+#define ACMD13 13
+#define ACMD13_R R2
+/* Get the number of written write blocks (Minus errors ) */
+
+#define ACMD22 22
+#define ACMD22_R R1
+/* Set the number of write blocks to be pre-erased before writing */
+#define ACMD23 23
+#define ACMD23_R R1
+/* Get the card's OCR (SD mode) */
+#define ACMD41 41
+#define ACMD41_R R1
+/* Connect or disconnect the 50kOhm internal pull-up on CD/DAT[3] */
+#define ACMD42 42
+#define ACMD42_R R1
+/* Get the SD configuration register */
+#define ACMD51 42
+#define ACMD51_R R1
+
+// Represents a raw CID register as returned by the card
+typedef union sd_cid_register {
+    uint8_t data[16];
+    struct _values {
+        uint8_t manufacturer_id;
+        unsigned char oem_id[2];
+        unsigned char product_name[5];
+        uint8_t revision;
+        uint32_t serial_number;
+        uint16_t manufacture_date;
+        uint8_t crc7;
+    } values;
+
+} sd_cid_register_t;
+
+typedef union sd_csd_register {
+    uint8_t data[16];
+    
+} sd_csd_register_t;
+
+// A more program friendly version of the CID register
+typedef struct sd_card_info {
+    uint8_t manufacturer_id;
+    unsigned char oem_id[3];
+    unsigned char product_name[6];
+    uint8_t revision_major;
+    uint8_t revision_minor;
+    uint32_t serial_number;
+    uint8_t manufacture_month;
+    uint16_t manufacture_year;
+} sd_card_info_t;
+
+void inline sd_start();
+void inline sd_stop();
+void sd_pack_argument(uint8_t *argument, uint32_t value);
+uint8_t sd_crc7(uint8_t crc, uint8_t data);
+uint16_t sd_read_register(uint8_t reg, uint8_t *buffer);
+uint8_t sd_command(unsigned char cmd, unsigned char *argument, uint8_t response_len);
+uint16_t sd_block_read(uint8_t *dest, uint16_t count);
+
+extern unsigned char spi_byte(uint8_t);
+extern void spi_read(unsigned char*, unsigned char);
+extern void inline spi_idle(unsigned char);
+
+// CONFIGURATION
+//#define SD_CRC_ENABLED
+
+// Pin configuration
+#define SD_CS  LATC2
