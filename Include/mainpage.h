a15477098f93dbadb1b8f12a19038cd502d1fde6
David <david@edeca.net>
2015-11-01 16:00:02 +0000

    Update doxygen license date

--- Include/mainpage.h
+++ Include/mainpage.h
@@ -14,7 +14,7 @@ The source code repository may be browsed at https://github.com/edeca/Electronic
 @section license License
 Unless otherwise stated, all code is released under the BSD license below.
 
-Copyright (c) 2010-2012, David Cannings
+Copyright (c) 2010-2015, David Cannings
 
 All rights reserved.
 
